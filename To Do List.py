250be4ecc545b8eff4e366c562d8e7d810bfdd2d
Lia Lubit <lialubit@login12.chn.perlmutter.nersc.gov>
2026-05-31 11:27:47 -0700

    modify general functions to be workable with and without the emulator, map out next steps

--- To Do List.py
+++ To Do List.py
@@ -2,12 +2,20 @@ COBAYA PROJECT TODO LIST
 1. Confirm P(k) emulator is working
 2. get from P(k) emulator to relevant spectra
 3. test time diff and accuracy diff from PyCCL
-3.5. Should I be using weaklensingtraer for all my galaxy source bins?
-3.6. Change everything to use A_s instead of sigma8
-4. build this into Cobaya -- new likelihood?
-5. spawn more chains and use NERSC exclusive node
-6. rerun omega_m and check results
-7. run for omega_m and sigma_8 --> if lensing ratios are working as we think, we should not be constraining sigma8 at all (or a_s)
+
+QUESTION: Should I be using weaklensingtraer for all my galaxy source bins?
+
+1. Modify all standing functions to be workable with or without emulator -- DONE
+2. Change calculate and plot functions to be workable with or without emulators
+3. Change all cosmologies and functions to use A_s instead of sigma8 -- this will involve changing functions that use sigma8 to be ok with A_s 
+-- might be worth having them depend on whether an emulator is being used or not
+4. test covariance matrices with and without emulator
+5. modify classes to be workable with or without emulator
+6. make new likelihood that works with the emulator -- this will need the new cosmology
+7. test omega_m with Cobaya
+8. spawn more chains and use NERSC exclusive node
+9. rerun omega_m and check results
+10. run for omega_m and sigma_8 --> if lensing ratios are working as we think, we should not be constraining sigma8 at all (or a_s)
 
 Later
 1. Make trace and corner plot functions in functions_and_classes.py to make this clean
