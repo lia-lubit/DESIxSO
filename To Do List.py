3c93877fd175314dc10bd92b3bdbbf6b166e280c
Lia Lubit <lialubit@x1004c5s6b0n0h0.chn.perlmutter.nersc.gov>
2026-06-08 12:30:16 -0700

    emulated MCMC run for omega_m and A_s

--- To Do List.py
+++ To Do List.py
@@ -1,17 +1,15 @@
 COBAYA PROJECT TODO LIST
-0. get chi 2 for diff omega_m
-    minimizer?
-visualize spectra for diff omega_m? not sure what this was about 
 1. organize
-2. figure out why its not working with the emulator and fix
+2. run for omega_m and sigma_8 --> if lensing ratios are working as we think, we should not be constraining sigma8 at all (or a_s)
+3. run for omega_m and sigma_8 with slight variation in starting points (right now its running with them all starting at the same time)
+4. figure out what we expect given degeneracy
 
-Later
-0. run for omega_m and sigma_8 --> if lensing ratios are working as we think, we should not be constraining sigma8 at all (or a_s)
-1. Make trace and corner plot functions in functions_and_classes.py to make this clean
-2. Make covariance and data saving function?
 
 Other
+visualize spectra for diff omega_m? not sure what this was about 
 change to uniform prior?
 marco bonicci
 if pyccl is still slow after emulating the matter power spectrum, consider implementing the integrals ourselves 
 once we do this, we can start exploring different configurations and start seeing which ones are most interesting and start analyzing data
+1. Make trace and corner plot functions in functions_and_classes.py to make this clean
+2. Make covariance and data saving function?
